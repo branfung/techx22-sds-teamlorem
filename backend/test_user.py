39435dac92610e201a285cf8f26ac863308e806a
branfung <branfung@hotmail.com>
2022-03-03 23:06:04 -0400

    fixed unit test bug

--- backend/test_user.py
+++ backend/test_user.py
@@ -1,12 +1,13 @@
-from unittest import TestCase
+import unittest
 from objects.user import User
 
 
-def TestUser(TestCase):
-    
-    user1 = User('BRANDON')
-    
-    def test_user(self):
+class TestUser(unittest.TestCase):
+    def test_user_init(self):
+        user1 = User('BRANDON')
+
         self.assertEqual(user1, 'My name is Brandon.')
+        self.assertRaises(User(45), TypeError)
 
-
+if __name__ == '__main__':
+    unittest.main()
