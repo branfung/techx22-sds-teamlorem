5d36b35295b51288f97733bd190f427291bf585c
Yan Carlos Aquino Gutierrez <yan.aquino1@upr.edu>
2022-03-09 00:49:57 -0400

    forgot to add test_order.py changes to previous commit

--- backend/test_order.py
+++ backend/test_order.py
@@ -1,6 +1,6 @@
-from typing import Type
 import unittest
 # TODO - Import product class in order to to create product object to populate test_products
+# TODO - Import Order Class in order to create an order 
 #from objects.Product import Product
 
 test_products = None
@@ -15,7 +15,6 @@ test_city = 'Kissimmee'
 class TestOrder(unittest.TestCase):
 
     def test_argument_types(self):
-        self.assertRaises(TypeError,Order,test_products,test_street_address, test_zip_code,test_state,test_phone_number,test_email,test_city)
 
         # products must be a dictionary ? Like in inventory  
         self.assertRaises(TypeError, Order, [], test_street_address, test_zip_code, test_state, test_phone_number, test_email, test_city)
@@ -29,7 +28,24 @@ class TestOrder(unittest.TestCase):
         self.assertRaises(TypeError, Order, test_products, test_street_address, test_zip_code, test_state, test_phone_number, test_email, True)
     
     def test_update_order(self):
-        pass
+        # to_remove must be a boolean to_remove
+        self.assertRaises(TypeError, Order.update_order, self, 'False', 0, test_products)
+
+        # product_id must be an int
+        self.assertRaises(TypeError, Order.update_order, self, False , '12345', test_products)
+
+        # product_to_be_added must an instance of the Product class
+        self.assertRaises(TypeError, Order.update_order, self, False , 1234567891234567, 0)
+
+        # method succesfully remove the item from the order
+        test_order = Order()
+        test_order.update_order(test_products[0])
+        self.assertEquals(test_order.products, {})
+
+        # method succesfully adds the item to the order 
+        test_order2 = Order()
+        test_order2.update_order(test_products[0])
+        self.assertEquals(test_order2.products, {})
 
     def test_tracking(self):
         # tracking must be a string
